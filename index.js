73e34aa01229b54820647f067de526d22b0c0098
tw1997 <t888y@qq.com>
2018-07-27 21:40:08 +0800

    format code

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ const cp = require('child_process');
 var program = require('commander');
 
 program
-    .option('-p, --prot <n>', '设置端口')
-    .parse(process.argv);
+  .option('-p, --prot <n>', '设置端口')
+  .parse(process.argv);
 
 let port = 8080;
 
 if (program.prot) {
-    port = program.prot;
+  port = program.prot;
 }
 
 const httpServer = http.createServer(server(process.cwd(), port));
 
 httpServer.on('error', (err) => {
-    console.log(err);
+  console.log(err);
 });
 // 指定一个监听的接口
 httpServer.listen(port, () => {
-    console.log('runing...');
-    for (const ip of ips) {
-        console.log(`http://${ip}:${port}`);
-    }
-    // 打开默认浏览器
-    cp.exec(`start http://localhost:${port}`);
+  console.log('runing...');
+  for (const ip of ips) {
+    console.log(`http://${ip}:${port}`);
+  }
+  // 打开默认浏览器
+  cp.exec(`start http://localhost:${port}`);
 });
 // 监听文件是否改动
 f5Server(process.cwd(), httpServer);
